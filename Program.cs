896f59359aef7018effe1c9bfe2d6d26e138ff59
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-07-08 14:59:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Assignment_10._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] posArray = { 2, -1, 3, -3, 10, -200 };
+
+            var numQuery = from num in posArray where num > 0 select num;
+            foreach ( int num in numQuery )
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
